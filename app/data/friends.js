f2ea1a05dd0dd58ab0ae51dea46554b4195b7b54
Nick <nickkwon92@gmail.com>
2018-03-20 20:28:05 -0700

    working app

---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+var friends = [ {
+	"name" : "Nick",
+	"photo" : "https://cdn.discordapp.com/attachments/387278478589952000/420462340367187968/image.jpg",
+	"scores" : [5,5,2,3,1,5,2,1,3,5]
+}, {
+	"name" : "Brad",
+	"photo" : "https://cdn.discordapp.com/attachments/387278478589952000/425559354478166027/zmlu1t7mipm01.jpg",
+	"scores" : [1,5,1,1,1,1,1,1,5,5]
+}, {
+	"name" : "Jason",
+	"photo" : "https://cdn.discordapp.com/attachments/387278478589952000/425849549119291392/unknown.png",
+	"scores" : [5,3,4,3,2,5,1,3,3,4]
+}];
+
+
+module.exports = friends;
